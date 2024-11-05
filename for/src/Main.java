cedb836941bea0336232ae762c4573d3998f549a
Maxim800729 <maxchakanov@gmail.com>
2024-11-05 22:10:32 +0300

    task with for

---
+++ for/src/Main.java
@@ -0,0 +1,82 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My home work with for");
+
+       /* System.out.println("********");
+        System.out.println("********");
+        System.out.println("********");
+        System.out.println("********");*/
+
+        int height = 5;
+        int width = 8;
+        for (int i = 0; i < height; i++) {
+            for (int g = 0; g < width; g++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //System.out.println("*");
+        //System.out.println(" *");
+        //System.out.println("  *");
+        //System.out.println("   *");
+        //System.out.println("    *");
+        //System.out.println("     *");
+        //System.out.println("      *");
+        //System.out.println("       *");
+
+        int size = 8;
+
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                if (y == x) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+
+                for (int y = 0; y < size; y++) {
+                    for (int x= 0; x < size; x++) {
+                        if ( y + x ==size-1 ) {
+                            System.out.print("*");
+                        } else {
+                            System.out.print(" ");
+                        }
+                    }
+                    {
+                    }System.out.println();}
+
+
+                  /*System.out.println("*******");
+                    System.out.println("******");
+                    System.out.println("*****");
+                    System.out.println("****");
+                    System.out.println("***");
+                    System.out.println("**");
+                    System.out.println("*");*/
+                    {
+                     System.out.println();
+
+                    int size1 = 7;
+
+                    for (int u = 0; u < size1; u++){
+                        for (int w= 0; w < size1-u; w++){
+                            System.out.print("*");}
+
+                        System.out.println();
+
+
+
+
+                        }}}}
+
+
+
